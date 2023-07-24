659f045946dabb41df5bff0fd866aae60a252b56
Sariolka <119030348+Sariolka@users.noreply.github.com>
2023-07-24 14:40:56 +0300

    t

--- backend/app.js
+++ backend/app.js
@@ -18,25 +18,23 @@ const limiter = rateLimit({
 });
 const { PORT = 3000 } = process.env;
 const app = express();
-
+app.use(express.json());
+app.use(requestLogger);
 app.use(cors({
   origin: ['https://sariolka.student.nomoredomains.xyz', 'https://api.sariolka.students.nomoredomains.xyz',
     'http://sariolka.student.nomoredomains.xyz', 'http://api.sariolka.students.nomoredomains.xyz', 'localhost:3000',
     'http://localhost:3000', 'https://localhost:3000', 'localhost:3001', 'http://localhost:3001'],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 }));
 
 app.use(helmet());
 app.use(limiter);
 mongoose.connect('mongodb://localhost:27017/mestodb', { family: 4 });
-app.use(express.json());
-app.use(requestLogger);
 
 app.use(router);
 app.use(errorLogger);
 app.use(errors());
 app.use(error);
 app.listen(PORT, () => {
-  console.log('Сервер запущен!');
+  console.log(`Сервер запущен на ${PORT} порту!`);
 });
